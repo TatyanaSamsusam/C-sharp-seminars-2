0003df1e3f4bbdf0ba432f90c37502c225296d04
Tatyana <android87@list.ru>
2022-05-12 23:05:20 +0400

    checked if 2 numbers are kvadrats

---
+++ task_10_kavdratLi/Program.cs
@@ -0,0 +1,12 @@
+﻿// 16. Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA * numberA == numberB) Console.WriteLine("yes");
+else if (numberB * numberB == numberA) Console.WriteLine("yes");
+else Console.WriteLine("no");
