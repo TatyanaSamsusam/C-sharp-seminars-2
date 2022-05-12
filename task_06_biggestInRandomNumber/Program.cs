cc32a35a05f85bdf18ccd4f80551fcd72ab82ec9
Tatyana <android87@list.ru>
2022-05-12 21:25:43 +0400

    find the biggest digit in random number

---
+++ task_06_biggestInRandomNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+int number = new Random().Next(1,100);
+Console.WriteLine($"random number is {number}");
+int firstDigit = number / 10;
+Console.WriteLine(firstDigit);
+int secondDigit = number % 10;
+Console.WriteLine(secondDigit);
+
+Console.WriteLine(firstDigit > secondDigit ? $"max digit is {firstDigit}" : $"max digit is {secondDigit}");
