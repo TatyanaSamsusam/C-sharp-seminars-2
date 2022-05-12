99699752e37fbe8b55e8e3271b9f39141daca391
Tatyana <android87@list.ru>
2022-05-12 22:11:51 +0400

    delete a digit from the middle

---
+++ task_07_deleteMiddleNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100,1000);
+Console.WriteLine($"random number is {number}");
+
+int lastDigitMethod (int number)
+{
+    int firstDigit = number % 10;
+    int lastDigit = number /100 * 10 + firstDigit;
+    return lastDigit;
+}
+
+int digitMethod = lastDigitMethod (number);
+
+
+Console.WriteLine(digitMethod);
