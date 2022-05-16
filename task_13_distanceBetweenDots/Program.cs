36fb6debd12277db3223ddea6762c3fe0217b7fc
Tatyana <android87@list.ru>
2022-05-16 22:27:41 +0400

    found distance between dots

---
+++ task_13_distanceBetweenDots/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("введите координаты точки A");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите координаты точки B");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+Console.WriteLine(result);
