010cd42f25a781be74b49020171b69c574df4e7e
Tatyana <android87@list.ru>
2022-05-19 22:40:13 +0400

    proizvedenie i random array

---
+++ task15_howManyDigitsInNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int CountNum (int number)
+{
+    int count = 0;
+    while(number > 0)
+    {
+        number /=10;
+        count ++;
+    }
+    return count;
+}
+int num = Convert.ToInt32(Console.ReadLine());
+int count = CountNum(num);
+Console.WriteLine($"в числе {num} - {count} цифр");
---
+++ task16_proizvedenie/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24
+5 -> 120*/
+
+Console.WriteLine("введи число, и я покажу произведение от 1 до твоего числа");
+int num = int.Parse(Console.ReadLine());
+
+int FindMultiple(int number)
+{
+    int mult = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        mult = mult * i;
+    }
+    return mult;
+}
+
+int result = FindMultiple(num);
+Console.WriteLine($"произведение чисел от 1 до {num} равно {result}");
+
