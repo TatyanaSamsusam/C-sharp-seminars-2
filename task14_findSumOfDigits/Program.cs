341b40860d3cc94c6a61395079356321a30c8983
Tatyana <android87@list.ru>
2022-05-19 21:30:22 +0400

    finds sum with methods

---
+++ task14_findSumOfDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.WriteLine("введи число, и я покажу сумму от 1 до твоего числа");
+int num = int.Parse(Console.ReadLine());
+
+int FindSum(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int result = FindSum(num);
+Console.WriteLine($"сумма чисел от 1 до {num} равна {result}");
