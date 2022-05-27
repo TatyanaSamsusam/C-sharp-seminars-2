a8edf80bc3f84ec72a21462a7e6c4817c7223b15
Tatyana <android87@list.ru>
2022-05-27 15:17:29 +0400

    find a digit in array

---
+++ task20_findNumberInArray/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+//-3; массив [6, 7, 19, 345, 3] -> да
+
+Console.WriteLine("введите размер массива:");
+int size = int.Parse(Console.ReadLine());
+Console.WriteLine("какое число ищем? ");
+int find = int.Parse(Console.ReadLine());
+string result = "нет";
+
+int[] array = new int[size];
+var NewRnd = new Random();
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = NewRnd.Next(1,10);
+    Console.Write(array[i] + " ");
+}
+
+for (int i = 0; i < size; i++)
+{
+    if (array[i] == find) result = "да";
+}
+Console.WriteLine(result);
