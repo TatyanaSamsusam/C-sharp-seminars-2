7d332c210e468bef24c1a7926d23373377d7b0d8
Tatyana <android87@list.ru>
2022-05-29 12:48:20 +0400

    prerevod v dvoichuyu sistemu recursiej

---
+++ task26_perevodVDvoichnoeRecursiya/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+Console.WriteLine("введите число в десятичной системе: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("введите основание системы счисления: ");
+int baseNum = int.Parse(Console.ReadLine());
+
+void DecToOther (int n, int baseN)
+{
+    if (n == 0) return;
+    DecToOther ( n / baseN, baseN);
+    Console.Write(n % baseN);
+}
+
+DecToOther(num, baseNum);
