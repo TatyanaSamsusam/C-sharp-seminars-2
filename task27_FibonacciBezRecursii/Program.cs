a08b6cbd03f89e61c0fa94d87aef0791bb0e511c
Tatyana <android87@list.ru>
2022-05-29 13:40:14 +0400

    fibonacci bez recursii

---
+++ task27_FibonacciBezRecursii/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8 */
+
+Console.Write("введите число: ");
+int fibNo = int.Parse(Console.ReadLine());
+int a = 0;
+int b = 1;
+
+Console.Write(a + " " + b + " ");
+int sum = 0;
+
+for (int i = 0; i < fibNo + 1; i++)
+{
+    sum = a + b;
+    Console.Write(sum + " ");
+    a = b;
+    b = sum;
+}
+
