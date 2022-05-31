f8d987b2f3d6b589369164031e5525f0585621a2
Tatyana <android87@list.ru>
2022-05-31 11:55:02 +0400

    change elements in array: upd

---
+++ task31_changeElementIn2Darr/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты */
+
+int[,] GetArray(int a, int b)
+{
+    int[,] array = new int[a, b];
+    Random rndNum = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rndNum.Next(10);
+        }
+    }
+    return array;
+}
+
+
+void PrintInitArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintModifiedArray (int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 ==0) array[i, j] = array[i, j]* array[i, j];
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] arr = GetArray(5, 7);
+PrintInitArray(arr);
+Console.WriteLine();
+PrintModifiedArray(arr);
