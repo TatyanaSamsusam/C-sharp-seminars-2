d2e1fc9c8c05ef0ab9cc4e042888ae229839644d
Tatyana <android87@list.ru>
2022-06-06 21:27:50 +0400

    show numbers from 1 to N using recursion

---
+++ task36_digitsFrom1ToNRec/Program.cs
@@ -0,0 +1,14 @@
+﻿// напиши программу, которая принимает на вход одно число (N), а на выходе показывает все целые числа в промежутке от 1 до N
+
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+
+NaturalNumber(n);
+Console.ReadKey();
+
+void NaturalNumber(int num)
+{
+    if (num == 0) return;
+    NaturalNumber(num-1);
+    Console.Write(num + " ");
+}
