2c1c303d11cfb855aa3e3437b1437995d46512be
Tatyana <android87@list.ru>
2022-06-06 22:21:23 +0400

    show nat numbers from M to N recursion

---
+++ task37_showNumbersRec2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N: 
+// M=1, N=5:  1,2,3,4,5
+// M=4, N=8:  4,5,6,7,8
+
+Console.WriteLine("Введите число M");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+
+PrintNatNum(m, n);
+Console.ReadKey();
+
+void PrintNatNum(int n, int m)
+{
+    if (n == m)
+    {
+        Console.Write($"{n} ");
+        return;
+    }
+    if (m == n)
+    {
+        PrintNatNum(n-1,m);
+        Console.Write($"{n} ");
+        return;
+    }   
+    else
+    {
+        PrintNatNum(n+1,m);
+        Console.Write($"{n} ");
+        return;
+    }
+}
