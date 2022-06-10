07b434b46d5457709b58fb5633292f864749f812
Tatyana <android87@list.ru>
2022-06-10 17:27:07 +0400

    find sum of digits using recursion

---
+++ task38_findSumofDigitsRec/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9 */
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+void GetSum(int num, int sum)
+{
+    if (num == 0) 
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+    sum = sum + num %10;
+    num = num / 10;
+    GetSum(num, sum);
+}
+GetSum(n,0);
