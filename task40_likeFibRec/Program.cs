bff43079f60ebb6c52b55960e087120c61ee4f9a
Tatyana <android87@list.ru>
2022-06-12 16:56:10 +0400

    show Fibonacci with recursion

---
+++ task40_likeFibRec/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 70: Напишите программу, которая на вход принимает два числа и выдаёт
+первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+3 и 4, N = 5 -> 3 4 7 11 18
+6 и 10, N = 4 -> 6 10 16 26 */
+
+Console.Write("введите число N: ");
+int N = int.Parse(Console.ReadLine());
+Console.Write("введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("введите число B: ");
+int b = int.Parse(Console.ReadLine());
+
+
+void FibRec (int a, int b, int count)
+{
+    int sum = 0;
+    if (count == 0) return;
+  
+    sum = a + b;
+    a = b;
+    b = sum;
+    Console.Write($"{sum} ");
+    FibRec(a, b, count-1);
+}
+
+Console.Write(a + " " + b + " ");
+FibRec(a, b, N-2);
