044ee9a6950c660dd48c4b5fda271bf63bf9ffaa
Tatyana <android87@list.ru>
2022-06-12 17:56:01 +0400

    makes words from small alphabeth

---
+++ task41_makeWords/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 71: В некотором машинном алфавите имеются четыре буквы «а», «и», «с» и «в». Покажите все слова, состоящие из n букв, которые можно построить из букв этого алфавита.
+n = 2 -> аа, ии, сс, вв, аи, иа, ис, си, ас, са,
+ав, ва, ви, ив, св, вс */
+
+string charsWords = "аисв";
+int WordsLength = 2;
+
+void PrintAllWords (string alphabeth, int length, string prefix)
+{
+    if (length == 0) Console.Write(prefix + " ");
+    else foreach (char c in alphabeth) PrintAllWords (alphabeth, length-1, prefix + c);
+}
+
+PrintAllWords(charsWords, WordsLength, "");
+Console.WriteLine();
